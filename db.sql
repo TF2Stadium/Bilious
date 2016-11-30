bb0909a4ac4b515bb31027bf3049cd40e0bb1edf
gpittarelli <tf2stadium@gjp.cc>
2016-11-30 18:53:46 -0500

    add db setup.sql

---
+++ db.sql
@@ -0,0 +1,20 @@
+CREATE TABLE tournaments (
+    id integer NOT NULL primary key,
+    created_at timestamp DEFAULT CURRENT_TIMESTAMP,
+    state integer default 1 NOT NULL,
+    type integer NOT NULL,
+    map_name character varying(255) NOT NULL,
+    region_code character varying(255),
+    region_name character varying(255),
+    steam_group character varying(255),
+    twitch_channel character varying(255),
+    twitch_restriction integer NOT NULL,
+    serveme_id integer NOT NULL,
+    server_info_id integer NOT NULL,
+    created_by_steam_id character varying(255) NOT NULL,
+    ready_up_timestamp bigint default NULL,
+    match_ended boolean default false NOT NULL,
+    whitelist character varying(255) NOT NULL,
+    region_lock boolean NOT NULL,
+    logstf_id integer
+);
